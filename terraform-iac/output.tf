f82788b84e069cbc4f18158e8f33979bdc5deb8d
mou.idri@gmail.com <mou.idri@gmail.com>
2020-06-14 16:16:45 +0200

    commit with terraform files upload

--- terraform-iac/output.tf
+++ terraform-iac/output.tf
@@ -1,9 +1,3 @@
 output "subscription_id" {
   value = "${data.azurerm_client_config.current.subscription_id}"
 }
-output "rg_name" {
-  value = "${azurerm_resource_group.rg.name}"
-}
-output "rg_id" {
-  value = "${azurerm_resource_group.rg.id}"
-}
