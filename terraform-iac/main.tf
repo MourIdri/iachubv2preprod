2235a086a93b3f2ce0652a654270e10bd4c2ec49
mou.idri@gmail.com <mou.idri@gmail.com>
2020-06-14 16:50:00 +0200

    commit with terraform files upload

--- terraform-iac/main.tf
+++ terraform-iac/main.tf
@@ -27,3 +27,30 @@ module "rg" {
     projectowner="it_transverse_cloud_team"
   }
 }
+
+module "logging" {
+  source               = "./modules/logging"
+  current-name-convention-core-public-module = "${var.current-name-convention-core-public-main}"
+  current-name-convention-core-module  = "${var.current-name-convention-core-main}"
+  preferred-location-module = "${var.preferred-location-main}"
+  tags-sto-logging-module = {
+    environment = "production"
+    scope_1="shared_infrastructure"
+    scope_2="core_infrastructure"
+    type_1="network_security"
+    type_2="storage"
+    lob="it_infrastructure"
+    business_location="corpc"
+    projectowner="it_transverse_cloud_team"}
+  stoc_depend_on_module = [module.rg ]
+  tags-repo-logging-module = {
+    environment = "production"
+    scope_1="shared_infrastructure"
+    scope_2="core_infrastructure"
+    type_1="network_security"
+    type_2="logging"
+    lob="it_infrastructure"
+    business_location="corpc"
+    projectowner="it_transverse_cloud_team"}
+  logacc_depend_on_module = [module.rg ]
+}
