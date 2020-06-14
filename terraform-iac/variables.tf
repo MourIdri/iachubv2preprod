3eeab19e55f924adb7a559feb6e63dfb8bef0c82
mou.idri@gmail.com <mou.idri@gmail.com>
2020-06-14 17:14:04 +0200

    commit with terraform files upload

--- terraform-iac/variables.tf
+++ terraform-iac/variables.tf
@@ -25,21 +25,21 @@
 
 variable "preferred-location-main" {
   description = "Location of the network"
-  default     = "westeurope"
+  type = any
 }
 
 variable "second-location-main" {
   description = "Location of the network"
-  default     = "northeurope"
+  type = any
 }
 variable "current-name-convention-core-main" {
   description = "Every ressources has a core model crpc-prod-shar-hub-subnet-publicdmzin "
-  default     = "cc-pp-hb"
+  type = any
 }
 
 variable "current-name-convention-core-public-main" {
   description = "Every PUBLIC accesed and resolved ressources has a core model crcprdshrhubstolog "
-  default     = "ccppdhb"
+  type = any
 }
 ############################
 # GENERAL NETWRK VARIABLES #
