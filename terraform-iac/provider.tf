7ea0370c3dcc218fd166dc2cad70b82096614495
mou.idri@gmail.com <mou.idri@gmail.com>
2020-09-03 13:24:46 +0200

    adding public IP for NVA

--- terraform-iac/provider.tf
+++ terraform-iac/provider.tf
@@ -1,18 +1,6 @@
 provider "azurerm" {
   version="2.0.0"
   features {}
-  #subscription_id = "${var.azure-subscription-id}"
-  #client_id       = "${var.azure-client-app-id}"
-  #client_secret   = "${var.azure-client-secret-password}"
-  #tenant_id       = "${var.azure-tenant-id}"
 }
 
-terraform {
-  required_version = "0.12.24"
-  backend "azurerm" {
-    storage_account_name = "srv__current-name-convention-core-public-main__tfsa"
-    container_name       = "terraform"
-    key                  = "terraform-__current-name-convention-core-public-main__.tfstate"
-    access_key           = "__tf_storage_account_key__"
-  }
-}
+
